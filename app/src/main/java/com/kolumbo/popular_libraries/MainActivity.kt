39e861615b6202fe640a51129b79162e79528469
Kolyanzz <guslyakov3343@gmail.com>
2021-11-03 20:47:29 +0900

    не закончил пока еще

--- app/src/main/java/com/kolumbo/popular_libraries/MainActivity.kt
+++ app/src/main/java/com/kolumbo/popular_libraries/MainActivity.kt
@@ -2,10 +2,40 @@ package com.kolumbo.popular_libraries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.kolumbo.popular_libraries.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainView {
+
+    private var vb: ActivityMainBinding? = null
+    val presenter = MainPresenter(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb?.root)
+
+        val listener = View.OnClickListener {
+            presenter.counterClick(it.id)
+        }
+
+        vb?.btnCounter1?.setOnClickListener(listener)
+        vb?.btnCounter2?.setOnClickListener(listener)
+        vb?.btnCounter3?.setOnClickListener(listener)
+
+
     }
+
+    //Подсказка к ПЗ: поделить на 3 отдельные функции и избавиться от index
+    override fun setCounter1(text: String) {
+        vb?.btnCounter1?.text = text
+    }
+    override fun setCounter2(text: String) {
+        vb?.btnCounter2?.text = text
+    }
+    override fun setCounter3(text: String) {
+        vb?.btnCounter3?.text = text
+    }
+
+
 }
---
+++ app/src/main/java/com/kolumbo/popular_libraries/MainView.kt
@@ -0,0 +1,7 @@
+package com.kolumbo.popular_libraries
+
+interface MainView {
+    fun setCounter1(text: String)
+    fun setCounter2(text: String)
+    fun setCounter3(text: String)
+}
